9c20d4cbfe2121c18aeb2d49b038c6935abfbb9b
Basil Al-Tamiim <basil.altamimi@intigral.net>
2018-01-30 01:34:01 +0400

    Add Activity Settings Menu Options

--- app/src/main/java/instagram/basil/com/instagramclone/Profile/AccountSettingsActivity.java
+++ app/src/main/java/instagram/basil/com/instagramclone/Profile/AccountSettingsActivity.java
@@ -3,16 +3,20 @@ package instagram.basil.com.instagramclone.Profile;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
 import instagram.basil.com.instagramclone.R;
+import instagram.basil.com.instagramclone.Utils.SectionsStatePagerAdapter;
 
 /**
  * Created by apple on 1/29/18.
@@ -24,14 +28,24 @@ public class AccountSettingsActivity extends AppCompatActivity {
 
     private Context mContext = AccountSettingsActivity.this;
 
+    private SectionsStatePagerAdapter pagerAdapter;
+    private ViewPager mViewPager;
+    private RelativeLayout mRelativeLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
         Log.d(TAG, "onCreate: started.");
 
+        mViewPager = findViewById(R.id.container);
+        mRelativeLayout = findViewById(R.id.relLayout1);
+
         // Setup settings list
         setupSettingsList();
+
+        // Setup Activity's fragments
+        setupFragments();
 
         // Setup Back Arrow
         setupBackArrow();
@@ -49,6 +63,19 @@ public class AccountSettingsActivity extends AppCompatActivity {
         });
     }
 
+    private void setupFragments() {
+        pagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new EditProfileFragment(), getString(R.string.edit_profile));
+        pagerAdapter.addFragment(new SignOutFragment(), getString(R.string.sign_out));
+    }
+
+    private void setViewPager(int fragmentNumber) {
+        mRelativeLayout.setVisibility(View.GONE);
+        Log.d(TAG, "setViewPager: navigating to fragment #: "+fragmentNumber);
+        mViewPager.setAdapter(pagerAdapter);
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
     private void setupSettingsList() {
         Log.d(TAG, "setupSettingsList: initializing 'Account Settings List'");
         ListView listView = findViewById(R.id.lvAccountSettings);
@@ -59,6 +86,17 @@ public class AccountSettingsActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
         listView.setAdapter(adapter);
+
+
+        // Show selected menu option
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: navigating to fragment #:" + position);
+                setViewPager(position);
+            }
+        });
+
     }
 
 }
---
+++ app/src/main/java/instagram/basil/com/instagramclone/Profile/EditProfileFragment.java
@@ -0,0 +1,27 @@
+package instagram.basil.com.instagramclone.Profile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import instagram.basil.com.instagramclone.R;
+
+/**
+ * Created by apple on 1/30/18.
+ */
+
+public class EditProfileFragment extends Fragment {
+
+    private static final String TAG = "EditProfileFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        return view;
+    }
+}
